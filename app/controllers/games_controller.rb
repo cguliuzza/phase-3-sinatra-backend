743f92726ea536782c3fe9581741f7dbc859f301
christinaguliuzza <christina.guliuzza@gmail.com>
2021-10-07 09:06:44 -0700

    add game controllers to jsonify data

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,40 @@
+class GamesController < ApplicationController
+
+    get '/games' do 
+        Game.all.to_json
+    end 
+
+    get '/games/:id' do
+        find_game
+        game.to_json
+    end
+
+    post '/games' do 
+        game = Game.create(
+            game_title: params[:game_title],
+            game_developer: params[:game_developer],
+            genre: params[:genre],
+            price: params[:price],
+            description: params[:description]
+        )
+        game.to_json
+    end 
+
+    patch'/games/:id' do
+        find_game
+        game.update(params)
+        game.to_json
+    end
+
+    delete '/games/:id' do
+        find_game
+        game.destroy
+    end
+
+private
+
+    def find_game
+        game = Game.find(params[:id])
+    end
+
+end 
